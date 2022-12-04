644c9196925158815cee45bdb5d68c52d3acfe17
MBA <mbarriosa339@gmail.com>
2022-12-04 12:57:35 +0100

    Fix: Project_v1
    - Clean Code

--- src/api_v1/services.py
+++ src/api_v1/services.py
@@ -16,16 +16,15 @@ def movements(top: list, position: list, instructions: str) -> list:
         if "L" in i:
             X, Y, coord = instruction.left()
             logs.info(X, Y, cardinal_points[coord])
-
         elif "R" in i:
             X, Y, coord = instruction.right()
             logs.info(X, Y, cardinal_points[coord])
-
         elif "M" in i:
             X, Y, coord = instruction.moving()
             logs.info(X, Y, cardinal_points[coord])
         else:
             logs.mov_error(i)
+
     check_coord(top, [X, Y, coord])
     output = [X, Y, cardinal_points[coord]]
     return output
@@ -35,11 +34,11 @@ class Instruccions:
     """
         C = [West, North, East, South]
         If mower rotates, its orientation is updated.
-        f mower moves forward, a position is added with respect to its orientation
+        If mower moves forward, a position is added with respect to its orientation
             West -> X = X - 1
             North -> Y = Y + 1
             East -> X = X + 1
-            South -> X = Y - 1
+            South -> Y = Y - 1
     """
 
     def __init__(self, X: int, Y: int, index: int):
